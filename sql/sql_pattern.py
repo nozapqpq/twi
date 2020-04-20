d6df35d5bc04a60e88a1628ea7071b7fcbc374fc
Nozaki <moyashimizu0831@gmail.com>
2020-04-20 23:54:26 +0900

    refs #16 main.csv用データの入出力関数を作成

--- sql/sql_pattern.py
+++ sql/sql_pattern.py
@@ -2,6 +2,7 @@
 import sql_manipulator
 import sql_jockey
 import csv
+import datetime
 
 class SQLPattern():
     def __init__(self, option=""):
@@ -60,16 +61,16 @@ class SQLPattern():
                 mean_level = mean_level+sp['level']
                 if sp['goal_order'] > 0:
                     mean_goal = mean_goal+sp['goal_order']
-            mean_value = mean_value/len(sim_pos)
-            mean_diff = mean_diff/len(sim_pos)
-            mean_level = mean_level/len(sim_pos)
-            mean_goal = mean_goal/len(sim_pos)
+            mean_value = round(mean_value/len(sim_pos),3)
+            mean_diff = round(mean_diff/len(sim_pos),3)
+            mean_level = round(mean_level/len(sim_pos),3)
+            mean_goal = round(mean_goal/len(sim_pos),3)
             sigma = 0
             for sp in sim_pos:
                 sigma = sigma + (sp[target_str]-mean_value)**2
                 #sigma = sigma + ((sp['horse_last3f']-sp['race_last3f'])-mean_value)**2
-            sigma = sigma/len(sim_pos)
-            diviation = -(self_data[target_str]-mean_value)/sigma*10+50
+            sigma = round(sigma/len(sim_pos),3)
+            diviation = round(-(self_data[target_str]-mean_value)/sigma*10+50,3)
             #diviation = -((self_data['horse_last3f']-self_data['race_last3f'])-mean_value)/sigma*10+50
             ret_dict = {"diviation":diviation,"sigma":sigma,"total":len(sim_pos),"mean_value":mean_value,"mean_diff":mean_diff,"mean_level":mean_level,"mean_goal":mean_goal,"diff3f":self_data['diff3f'],"horse_last3f":self_data['horse_last3f'],"class":self_data['class']}
         return ret_dict
@@ -290,6 +291,90 @@ class SQLPattern():
             if place == self.place_alpha[p]:
                 return self.place_kanji[p]
         return "unknown"
+
+    def set_single_maindata(self, entry, target, self_data, div_dict, goal):
+        single_maindata = []
+        single_maindata.append(target['rdate'])
+        single_maindata.append(target['race'])
+        single_maindata.append(target['place'])
+        single_maindata.append(target['turf_dirt'])
+        single_maindata.append(target['distance'])
+        single_maindata.append(entry['horsename'])
+        single_maindata.append(target['class'])
+        single_maindata.append(entry['jockey_name'])
+        single_maindata.append(entry['jockey_weight'])
+        single_maindata.append(entry['zi'])
+
+        single_maindata.append(entry['pastnum'])
+        single_maindata.append(str(self_data['rdate']))
+        single_maindata.append(self_data['race'])
+        single_maindata.append(self_data['place'])
+        single_maindata.append(self_data['turf_dirt'])
+        single_maindata.append(self_data['distance'])
+        single_maindata.append(self_data['class'])
+        single_maindata.append(self_data['course_condition'])
+        single_maindata.append(self_data['horseweight'])
+        single_maindata.append(self_data['rap3f'])
+        single_maindata.append(self_data['rap5f'])
+        single_maindata.append(self_data['diff3f'])
+        single_maindata.append(self_data['time_diff'])
+        single_maindata.append(div_dict['mean_diff'])
+        single_maindata.append(div_dict['mean_goal'])
+        single_maindata.append(div_dict['diviation'])
+        single_maindata.append(div_dict['sigma'])
+        single_maindata.append(div_dict['total'])
+        single_maindata.append(self_data['horse_last3f'])
+        single_maindata.append(self_data['race_last3f'])
+        single_maindata.append(goal)
+        return single_maindata
+
+    def get_maindata_dict_from_csv(self, csvfile):
+        main_dict_list = []
+        with open(csvfile, 'r') as f:
+            reader = csv.reader(f)
+            header_flg = 0
+            for row in reader:
+                if header_flg == 0:
+                    header_flg = 1
+                    continue
+                main_dict = {}
+                main_dict["today_rdate"] = datetime.datetime.strptime(row[0], '%Y-%m-%d')
+                main_dict["today_race"] = int(row[1])
+                main_dict["today_place"] = row[2]
+                main_dict["today_turf_dirt"] = row[3]
+                main_dict["today_distance"] = int(row[4])
+                main_dict["horsename"] = row[5]
+                main_dict["today_class"] = row[6]
+                main_dict["today_jockey_name"] = row[7]
+                main_dict["today_jockey_weight"] = float(row[8])
+                main_dict["today_zi"] = int(row[9])
+
+                main_dict["pastnum"] = int(row[10])
+                main_dict["past_rdate"] = datetime.datetime.strptime(row[11], '%Y-%m-%d')
+                main_dict["past_race"] = int(row[12])
+                main_dict["past_place"] = row[13]
+                main_dict["past_turf_dirt"] = row[14]
+                main_dict["past_distance"] = int(row[15])
+                main_dict["past_class"] = row[16]
+                main_dict["past_course_condition"] = row[17]
+                main_dict["past_horseweight"] = int(row[18])
+                main_dict["past_rap3f"] = float(row[19])
+                main_dict["past_rap5f"] = float(row[20])
+                main_dict["past_diff3f"] = float(row[21])
+                main_dict["past_time_diff"] = float(row[22])
+                analyse_avail_list = [0,0,0,0]
+                if float(row[25]) > 0:
+                    analyse_avail_list = [float(row[23]),float(row[24]),float(row[25]),float(row[26])]
+                main_dict["past_mean_diff"] = analyse_avail_list[0]
+                main_dict["past_mean_goal"] = analyse_avail_list[1]
+                main_dict["past_diviation"] = analyse_avail_list[2]
+                main_dict["past_sigma"] = analyse_avail_list[3]
+                main_dict["past_total"] = int(row[27])
+                main_dict["past_horse_last3f"] = float(row[28])
+                main_dict["past_race_last3f"] = float(row[29])
+                main_dict["today_goal"] = int(row[30])
+                main_dict_list.append(main_dict)
+        return main_dict_list
 
     def set_race_level(self):
         hoge = 3
