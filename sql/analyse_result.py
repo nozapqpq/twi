8273a5155ce893808b8f5ad3e15224c529fbac7a
Nozaki <moyashimizu0831@gmail.com>
2020-11-03 00:36:44 +0900

    refs #32 product周り整理

--- sql/analyse_result.py
+++ sql/analyse_result.py
@@ -21,7 +21,7 @@ from sklearn.metrics import confusion_matrix
 # place_listに入っている場所に対するディープラーニングを利用できる
 # ディープラーニングの性能を図りたいときはresearch_flg = True
 place_list = ["東京","京都","福島"]
-research_flg = True
+research_flg = False
 all_place_flg = True
 all_td_flg = False
 
@@ -33,6 +33,9 @@ class AnalyseResult():
         self.util = utility.Utility()
         self.json_name = "deep_model.json"
         self.h5_name = "deep_model.h5"
+        # csv出力用
+        self.result_list = []
+        self.research_result_list = []
 
     def set_deep_model_name(self, place_dict):
         place_list = ["札幌","函館","福島","新潟","中山","東京","中京","京都","阪神","小倉"]
@@ -94,26 +97,24 @@ class AnalyseResult():
             extra_list = []
         return todayinfo_list, extra_list
 
-    # 判定機をjsonから読み出しdeeplearning_result.csvを出力、性能判定も行う
+    # 判定機をjsonから読み出しdeeplearning_result.csv用リストに書き込み
     def get_deep_model(self):
         model = model_from_json(open(self.json_name,"r").read())
         model.load_weights(self.h5_name)
         print(self.json_name)
         return model
-    def output_deeplearning_result_to_csv(self , model, pred_x_np, todayinfo_lst, dim, extra_lst, place_dict, today_date=""):
+    def set_deeplearning_result(self , model, pred_x_np, todayinfo_lst, dim, extra_lst, place_dict, today_date=""):
         out_analyse_list = []
 
-        with open("../deeplearning_result.csv","a") as f:
-            writer = csv.writer(f)
-            all_score = model.predict(pred_x_np)
-            for i in range(len(pred_x_np)):
-                score = list(all_score[i])
-                #score = list(model.predict(pred_x_np[i].reshape(1,dim))[0])
-                extra = []
-                if extra_lst != []:
-                    extra = extra_lst[i]
-                    out_analyse_list.append(todayinfo_lst[i]+score+extra)
-                writer.writerow(todayinfo_lst[i]+score+extra)
+        all_score = model.predict(pred_x_np)
+        for i in range(len(pred_x_np)):
+            score = list(all_score[i])
+            #score = list(model.predict(pred_x_np[i].reshape(1,dim))[0])
+            extra = []
+            if extra_lst != []:
+                extra = extra_lst[i]
+                out_analyse_list.append(todayinfo_lst[i]+score+extra)
+            self.result_list.append(todayinfo_lst[i]+score+extra)
         if today_date != "":
             self.analyse_deeplearning_output(out_analyse_list,today_date, place_dict)
 
@@ -122,22 +123,21 @@ class AnalyseResult():
         if len(out_list) < 10:
             return
         places = list(set([x[2] for x in out_list]))
-        with open("../deeplearning_research_result.csv","a") as f:
-            # １着度高い順に並べ替え
-            out_list = sorted(out_list, reverse=True, key=lambda x: x[3])
-            writer = csv.writer(f)
-            writer.writerow([today_date,place_dict["place"],place_dict["turf_dirt"]]+self.get_analyse_result(places,out_list,"1着度","over",3,False))
-            writer.writerow([today_date,place_dict["place"],place_dict["turf_dirt"]]+self.get_analyse_result(places,out_list,"1着度","over",3,True))
+        # １着度高い順に並べ替え
+        out_list = sorted(out_list, reverse=True, key=lambda x: x[3])
+        self.research_result_list.append([today_date,place_dict["place"],place_dict["turf_dirt"]]+self.get_analyse_result(places,out_list,"1着度","over",3,False))
+        self.research_result_list.append([today_date,place_dict["place"],place_dict["turf_dirt"]]+self.get_analyse_result(places,out_list,"1着度","over",3,True))
 
     # deeplearning_result.csvへの出力結果を性能評価し、結果出力
     def get_analyse_result_title(self):
-        return ["date","場所","芝ダ","title","午前午後","総数","1着","2着","3着","連対率","複勝率","5位以内との連対回数","2〜5位総数","10位以内との連対回数","6〜10位総数","11位以下との連対回数","11位以下総数","total配当(馬連)","平均配当(馬連)"]
+        return ["date","場所","芝ダ","title","午前午後","総数","1着","2着","3着","連対率","複勝率","5位以内との連対回数","2〜5位総数","10位以内との連対回数","6〜10位総数","11位以下との連対回数","11位以下総数","total配当(馬連)","平均配当(馬連)","1〜3位ボックス馬連的中回数","1〜3位ボックスワイド的中回数","1〜3位ボックスtotal配当(馬連)"]
     def get_analyse_result(self, places, out_list, elem_name, direction, target, afternoon_flg):
-        count_dict = {"total":0,"1st":0,"2nd":0,"3rd":0,"with_5th":0,"5th_count":0,"with_10th":0,"10th_count":0,"with_11th":0,"11th_count":0}
+        count_dict = {"total":0,"1st":0,"2nd":0,"3rd":0,"with_5th":0,"5th_count":0,"with_10th":0,"10th_count":0,"with_11th":0,"11th_count":0,"box_quinella_count":0,"box_triple_count":0}
         # goal index in deeplearning_result.csv
         goal_index = 5
         dividend_index = 7
         total_dividend = 0
+        box_total_dividend = 0
         # 午前中のレースは4つ
         target_race_total = 4
         noon_comment = "午前"
@@ -162,7 +162,7 @@ class AnalyseResult():
                 if single_analyse_list == [] or len(single_analyse_list[0]) < 8 or single_analyse_list[0][goal_index] == 0:
                     continue
                 # 1位から1頭ずつ見ていく
-                single_one_two_count = 0
+                box_list = [0, 0, 0]
                 for j in range(len(single_analyse_list)):
                     # 1位に対する処理
                     if j == 0:
@@ -190,12 +190,29 @@ class AnalyseResult():
                             count_dict["with_10th"] = count_dict["with_10th"] + 1
                         else:
                             count_dict["with_11th"] = count_dict["with_11th"] + 1
+                    # 上位3頭ボックス
+                    if j < 3:
+                        box_list[j] = single_analyse_list[j][goal_index]
+                # 上位3頭ボックス処理
+                box_quinella = 0
+                box_triple = 0
+                for bl in box_list:
+                    if bl >= 1 and bl <= 3:
+                        box_triple = box_triple + 1
+                        count_dict["box_triple_count"] = count_dict["box_triple_count"] + 1
+                        if bl <= 2:
+                            box_quinella = box_quinella + 1
+                if box_triple >= 2:
+                    count_dict["box_triple_count"] = count_dict["box_triple_count"] + 1
+                if box_quinella >= 2:
+                    count_dict["box_quinella_count"] = count_dict["box_quinella_count"] + 1
+                    box_total_dividend = box_total_dividend + single_analyse_list[0][dividend_index]
         average_dividend = 0
         if count_dict["total"] > 0:
             average_dividend = round(total_dividend/count_dict["total"],0)
         print(average_dividend)
         print(count_dict)
-        return [elem_name, noon_comment, count_dict["total"], count_dict["1st"], count_dict["2nd"], count_dict["3rd"], self.util.get_quinella_rate(count_dict), self.util.get_double_win_rate(count_dict), count_dict["with_5th"], count_dict["5th_count"], count_dict["with_10th"], count_dict["10th_count"], count_dict["with_11th"], count_dict["11th_count"],total_dividend,average_dividend]
+        return [elem_name, noon_comment, count_dict["total"], count_dict["1st"], count_dict["2nd"], count_dict["3rd"], self.util.get_quinella_rate(count_dict), self.util.get_double_win_rate(count_dict), count_dict["with_5th"], count_dict["5th_count"], count_dict["with_10th"], count_dict["10th_count"], count_dict["with_11th"], count_dict["11th_count"],total_dividend,average_dividend,count_dict["box_quinella_count"],count_dict["box_triple_count"],box_total_dividend]
 
     # matplotでaccuracyとlossをプロットして出力
     def compare_TV(self, history):
@@ -237,7 +254,7 @@ class AnalyseResult():
         plt.show()
 
     # ディープラーニング本体
-    def deep_learning(self, x_train, y_train, dim, horsename_list, pred_x_np, todayinfo_lst):
+    def deep_learning(self, x_train, y_train, dim):
         model = Sequential()
         y_sm = y_train.sum(axis=0)
         pos = y_sm[0]
@@ -285,35 +302,26 @@ class AnalyseResult():
 # ======class AnalyseResult terminated======
 
 def process_as_product(ar, place_dict_list):
-    with open("../deeplearning_result.csv","w") as f:
-        writer = csv.writer(f)
-        writer.writerow(ar.dotp.get_output_list_title())
     for pdl in place_dict_list:
         ar.set_deep_model_name(pdl)
-        goal_list = []
         dir_list = ar.get_csv_dir_list()
-        todayinfo_lst, extra_lst = ar.get_todayinfo_list([str(dir_list[-1])],pdl)
-        dummy1, dummy2, dummy3, target = ar.dotp.make_deeplearning_data(ar.get_main_data_from_dir_list([str(dir_list[-1])],pdl),"machine_learning/deep_pattern.json")
-        pred_x_np = np.array(target)
-        dim = len(pred_x_np[0])
 
-        lst = ar.get_main_data_from_dir_list(dir_list,pdl)
-        learn_lst, ans_lst, hn_lst, dummy_target = ar.dotp.make_deeplearning_data(lst,"machine_learning/deep_pattern.json")
+        race_data = ar.get_main_data_from_dir_list(dir_list,pdl)
+        learn_lst, ans_lst, dummy1, target = ar.dotp.make_deeplearning_data(race_data,"machine_learning/deep_pattern.json")
+        dim = len(target[0])
         # 着順分類リスト作成
+        goal_list = []
         for i in range(len(learn_lst)):
-            gl = ar.dotp.convert_fullgate_goal_list(ans_lst[i][0],ans_lst[i][1],ans_lst[i][2])
-            goal_list.append(gl)
-        # 各リストのnumpy化
+            goal_list.append(ar.dotp.convert_fullgate_goal_list(ans_lst[i][0],ans_lst[i][1],ans_lst[i][2]))
+
+        dim = len(target[0])
         x_np = np.array(learn_lst)
         y_np = np.array(goal_list)
+        ar.deep_learning(x_np, y_np, dim)
 
-        # ディープラーニング
-        ar.deep_learning(x_np, y_np, dim, hn_lst, pred_x_np, todayinfo_lst)
-
-        # 結果出力
+        # 精度確認用にconfusion_matrixをプロット
         model = ar.get_deep_model()
         ar.plot_confusion_matrix(y_np, model.predict(x_np), 0.8)
-        ar.output_deeplearning_result_to_csv(model, pred_x_np, todayinfo_lst, dim, extra_lst, pdl)
 
 def process_as_research(ar, place_dict_list):
     with open("../deeplearning_research_result.csv","w") as f:
@@ -334,7 +342,7 @@ def process_as_research(ar, place_dict_list):
             if len(target) == 0:
                 continue
             dim = len(pred_x_np[0])
-            ar.output_deeplearning_result_to_csv(model, pred_x_np, todayinfo_lst, dim, extra_lst, pdl, tl)
+            ar.set_deeplearning_result(model, pred_x_np, todayinfo_lst, dim, extra_lst, pdl, tl)
 
 def make_usedlset_dictlist(place_list, all_place_flg, all_td_flg):
     dict_list = []
