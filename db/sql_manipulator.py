c66e6a7e25acc10bb71d4654327bbb7089118684
noza <moyashimizu0831@gmail.com>
2021-01-31 23:50:07 +0900

    refs #9 学習時、入力データ作成を行わない選択肢を追加

--- db/sql_manipulator.py
+++ db/sql_manipulator.py
@@ -6,21 +6,19 @@ import sys
 print(sys.getdefaultencoding())
 class SQLManipulator():
     def __init__(self, ):
-        i=0
+        a=0
 
     def sql_manipulator(self, msg):
-        connection = MySQLdb.connect(
+        self.connection = MySQLdb.connect(
             host='localhost',
             user='noza',
             passwd='Pass_123',
             db='horse',
             use_unicode=True,
             charset='utf8')
-        cursor = connection.cursor()
-
+        cursor = self.connection.cursor()
         cursor.execute(msg)
         retval = cursor.fetchall()
-        connection.commit()
-        connection.close()
+        self.connection.close()
 
         return retval
--- main.sh
+++ main.sh
@@ -8,21 +8,22 @@ if [ $# -ne 1 ]; then
     exit 1
 fi
 
+rm -rf /home/noza/keiba/twi/1*.csv*
+rm -rf /home/noza/keiba/twi/2*.csv*
 if [ $1 = "jv_target" ]; then
     nkf -wd /media/noza/UBUNTU/source/jv_target.csv > ./jv_target_tmp.csv
     python3 set_jvtarget_to_db.py
-elif [ $1 = "train" ]; then
-    rm -rf /home/noza/keiba/twi/1*.csv*
-    rm -rf /home/noza/keiba/twi/2*.csv*
+elif [ $1 = "train_init" ]; then
     cp -rf /media/noza/UBUNTU/product/*.csv /home/noza/keiba/twi/
     for f in ./[0-9]*.csv;
         do nkf -wd $f > ${f}_tmp;
     done
     python3 dl_input_maker.py
     python3 train.py
+elif [ $1 = "train" ]; then
+    cp -rf /media/noza/UBUNTU/deep_model/* /home/noza/keiba/twi/
+    python3 train.py
 elif [ $1 = "predict" -o $1 = "honban" ]; then
-    rm -rf /home/noza/keiba/twi/1*.csv* 
-    rm -rf /home/noza/keiba/twi/2*.csv*
     if [ $1 = "predict" ]; then
         cp -rf /media/noza/UBUNTU/research/*.csv /home/noza/keiba/twi/
     elif [ $1 = "honban" ]; then
